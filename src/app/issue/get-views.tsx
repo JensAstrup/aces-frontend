f351863c76cb0b589aa066ffb080d32f53c76786
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-07-17 11:08:17 -0400

    ACEF-7: Add dropdown component for views (#8)
    * Add dropdown component, test coverage
    
    * Lint generated file, add express types

---
+++ src/app/issue/get-views.tsx
@@ -0,0 +1,17 @@
+interface View {
+    id: number
+    name: string
+}
+
+async function getFavoriteViews(token: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/views`, {
+    headers: {
+      Authorization: token,
+    },
+  })
+  const data = await response.json()
+  return data as View[]
+}
+
+export default getFavoriteViews
+export type { View }
--- src/components/ui/issue-description.tsx
+++ src/components/ui/issue-description.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import ViewDropdown from '@aces/components/view-dropdown'
+
 
 const IssueDescription: React.FC = () => {
   return (
     <div>
-      <h2 className="text-2xl font-bold">Issue Description</h2>
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold">Issue Description</h2>
+        <ViewDropdown />
+      </div>
       <div className="mt-2 prose text-muted-foreground">
         <p className="border rounded-md p-4 bg-muted/50 hover:bg-muted cursor-pointer">
           Implement a new feature that allows users to track their daily water intake. The feature should include
