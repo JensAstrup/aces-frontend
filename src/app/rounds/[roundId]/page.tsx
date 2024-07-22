a9f15141adeaa675e67af3c3fadb8899034bcc97
Jens Astrup <jensaiden@gmail.com>
2024-07-22 19:36:50 -0400

    Remove unnecessary newline

--- src/app/rounds/[roundId]/page.tsx
+++ src/app/rounds/[roundId]/page.tsx
@@ -27,7 +27,6 @@ function RoundPage({ params }: RoundPageProps) {
     }
   }, [favoriteViews, setSelectedView])
 
-
   return (
     <div className="grid md:grid-cols-5 gap-6 lg:gap-12 items-start max-w-6xl px-4 mx-auto py-6">
       <div className="md:col-span-3">
