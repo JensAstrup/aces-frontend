bafcff09e08f175f1b9f36c68ec3efe256b72fd8
Jens Astrup <jensaiden@gmail.com>
2024-07-26 16:56:49 -0400

    Consolidate oauth redirect page

--- src/app/oauth/callback/page.tsx
+++ src/app/oauth/callback/page.tsx
@@ -1,9 +1,22 @@
-import dynamic from 'next/dynamic'
+'use client'
+import React from 'react'
 
-
-const OAuthRedirect = dynamic(() => import('@aces/app/oauth/callback/OAuthComponent'), { ssr: false })
+import { Icons } from '@aces/components/icons'
+import useOAuthRedirect from '@aces/lib/hooks/use-oauth-redirect'
 
 
 export default function OAuthCallbackPage() {
-  return <OAuthRedirect />
+  useOAuthRedirect()
+
+  return (
+    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-md text-center justify-center items-center">
+        <h1 className="mt-4 text-4xl font-bold tracking-tight sm:text-5xl">Ace of Spades</h1>
+        <p className="mt-10 text-xl"> Authenticating... </p>
+        <div className="flex justify-center mt-4">
+          <Icons.spinner className="h-8 w-8 animate-spin" />
+        </div>
+      </div>
+    </div>
+  )
 }
