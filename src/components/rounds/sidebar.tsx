744237613498ddf51a280aab0716d21ad3627459
Jens Astrup <jensaiden@gmail.com>
2024-07-26 20:59:57 -0400

    Add loading spinner

--- src/components/rounds/sidebar.tsx
+++ src/components/rounds/sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Estimate } from '@aces/components/estimate'
+import { Icons } from '@aces/components/icons'
 import { Stats } from '@aces/components/ui/stats'
 import { Votes } from '@aces/components/votes'
 import { Issue } from '@aces/interfaces/issue'
@@ -14,7 +15,11 @@ interface RoundSidebarProps {
 
 export function RoundSidebar({ roundId, currentIssue, isIssueLoading }: RoundSidebarProps): JSX.Element {
   if (isIssueLoading) {
-    return <div>Loading issue data...</div>
+    return (
+      <div className="flex justify-center items-center min-h-[200px] h-full">
+        <Icons.spinner className="animate-spin h-8 w-8 text-gray-500" />
+      </div>
+    )
   }
 
   if (!currentIssue) {
