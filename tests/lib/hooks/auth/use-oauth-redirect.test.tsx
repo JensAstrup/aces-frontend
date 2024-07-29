c07656ae4a3d946c451b14aecb5595146df77def
Jens Astrup <jensaiden@gmail.com>
2024-07-29 00:26:46 -0400

    Lint removal

--- tests/lib/hooks/auth/use-oauth-redirect.test.tsx
+++ tests/lib/hooks/auth/use-oauth-redirect.test.tsx
@@ -1,6 +1,5 @@
-import { act, renderHook, waitFor } from '@testing-library/react'
+import { renderHook, waitFor } from '@testing-library/react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React from 'react'
 
 import useAuth from '@aces/lib/hooks/auth/use-authenticate'
 import useOAuthRedirect from '@aces/lib/hooks/auth/use-oauth-redirect'
