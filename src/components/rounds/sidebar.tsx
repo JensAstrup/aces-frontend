ee06dfd298bee233b2e6d00899b78f1f3f4bbfb4
Jens Astrup <jensaiden@gmail.com>
2024-08-03 13:05:30 -0400

    Simplify sidebar by utilizing WebsocketProvider

--- src/components/rounds/sidebar.tsx
+++ src/components/rounds/sidebar.tsx
@@ -4,9 +4,8 @@ import { Estimate } from '@aces/components/estimate'
 import { Icons } from '@aces/components/icons'
 import { Stats } from '@aces/components/ui/stats'
 import { Votes } from '@aces/components/votes'
-import { useUser } from '@aces/lib/hooks/auth/user-context'
+import WebSocketProvider from '@aces/components/web-socket-provider'
 import { useIssues } from '@aces/lib/hooks/issues/issues-context'
-import useWebSocketIssue from '@aces/lib/hooks/issues/use-websocket-issue'
 
 
 interface RoundSidebarProps {
@@ -14,19 +13,10 @@ interface RoundSidebarProps {
 }
 
 export function RoundSidebar({ roundId }: RoundSidebarProps): JSX.Element {
-  const { state: issuesState } = useIssues()
-  const { user } = useUser()
-  const { isLoading, issues, currentIssueIndex } = issuesState
-  const { issue: socketIssue } = useWebSocketIssue(roundId)
-  let currentIssue = null
-  if (!user) {
-    currentIssue = socketIssue
-  }
-  else {
-    currentIssue = issues[currentIssueIndex]
-  }
+  const { currentIssue } = useIssues()
+  console.log('currentIssue', currentIssue)
 
-  if (isLoading || !currentIssue) {
+  if (!currentIssue) {
     return (
       <div className="flex justify-center items-center min-h-[200px] h-full">
         <Icons.spinner className="animate-spin h-8 w-8 text-gray-500" />
@@ -36,6 +26,7 @@ export function RoundSidebar({ roundId }: RoundSidebarProps): JSX.Element {
 
   return (
     <>
+      <WebSocketProvider roundId={roundId} />
       <Estimate roundId={roundId} issue={currentIssue} />
       <Votes />
       <Stats />
