40518e4725e97cc66e2abb3e968e10ecea842092
Jens Astrup <jensaiden@gmail.com>
2024-09-13 00:19:19 -0400

    Remove unnecessary logging

--- src/app/api/auth/migrate/route.ts
+++ src/app/api/auth/migrate/route.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import * as Sentry from '@sentry/nextjs'
 import cookieSignature from 'cookie-signature'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
@@ -12,7 +13,6 @@ async function POST() {
   const cookieStore = cookies()
   const session = await getSession()
 
-  // Check for express-session cookie
   const expressSessionId = cookieStore.get('connect.sid')
   if (expressSessionId) {
     try {
@@ -28,9 +28,6 @@ async function POST() {
         }
       }
 
-      console.log('Decrypted sessionId:', sessionId)
-
-      // Find session in Prisma using the decrypted sessionId
       const expressSession = await prisma.session.findUnique({
         where: { id: sessionId },
       })
@@ -40,20 +37,13 @@ async function POST() {
       }
 
       const data = expressSession.data
-      console.log('Express session data:', data)
       const { user, anonymous } = JSON.parse(data)
-      console.log('User:', user)
-      console.log('Anonymous:', anonymous)
       session.user = user
       session.anonymous = anonymous
       await session.save()
-
-      console.log('Session migrated successfully')
-      console.log('Session:', session)
-      console.log('User:', session.user)
     }
     catch (error) {
-      console.error('Failed to migrate session:', error)
+      Sentry.captureException(error)
       return NextResponse.json({ message: 'Failed to migrate session' }, { status: 500 })
     }
   }
