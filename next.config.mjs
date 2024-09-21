ebcbd21f82d023e3f3ad634f088256712fb5d2f4
Jens Astrup <jensaiden@gmail.com>
2024-09-21 17:37:15 -0400

    Allow source maps and Sentry on staging

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,8 @@ const nextConfig = {}
 
 const isSentryEnabled = () => {
   const env = process.env.VERCEL_ENV
-  const gitRef = process.env.VERCEL_GIT_COMMIT_REF
 
-  return env === 'production' || (env === 'preview' && gitRef === 'develop')
+  return env === 'production' || (env === 'staging')
 }
 
 const sentryOptions = {
@@ -19,7 +18,7 @@ const sentryOptions = {
   project: process.env.SENTRY_PROJECT,
   silent: !process.env.CI,  // Suppresses Sentry logging during build when not in CI environment
   widenClientFileUpload: true,  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  hideSourceMaps: true,  // Hides source maps from generated client bundles
+  hideSourceMaps: process.env.VERCEL_ENV === 'production',  // Hides source maps from generated client bundles
   disableLogger: true,  // Automatically tree-shake Sentry logger statements to reduce bundle size
   automaticVercelMonitors: true,  // Enables automatic instrumentation of Vercel Cron Monitors
   release: packageJson.version  // Sets the release version to the version specified in package.json
