7352aec86bf2828f987376d0be7da35950413fd8
Jens Astrup <jensaiden@gmail.com>
2024-09-22 00:14:05 -0400

    Move ironOption

--- src/lib/server/auth/session.ts
+++ src/lib/server/auth/session.ts
@@ -5,22 +5,21 @@ import { getIronSession, IronSession } from 'iron-session'
 import { cookies } from 'next/headers'
 
 
-const ironOptions = {
-  cookieName: 'aces_session',
-  password: process.env.COOKIE_SECRET!,
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
-    sameSite: 'strict'
-  },
-}
-
 type SessionData = {
   user: User | null
   anonymous: boolean
 }
 
 async function getSession(): Promise<IronSession<SessionData>> {
+  const ironOptions = {
+    cookieName: 'aces_session',
+    password: process.env.COOKIE_SECRET!,
+    cookieOptions: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      sameSite: 'strict'
+    },
+  }
   console.log(`Retrieving session using cookie secret: ${process.env.COOKIE_SECRET}`)
   const cookieStore = cookies()
   const session = await getIronSession<SessionData>(cookieStore, ironOptions)
