87859312c46ba85efde6a919963d51f498e832a4
Jens Astrup <jensaiden@gmail.com>
2024-09-25 16:03:31 -0400

    Use server and move iron options into function

--- src/lib/server/auth/session.ts
+++ src/lib/server/auth/session.ts
@@ -1,24 +1,9 @@
+'use server'
 import 'server-only'
 import { User } from '@prisma/client'
 import { getIronSession, IronSession } from 'iron-session'
 import { cookies } from 'next/headers'
 
-
-const cookieSecret = process.env.COOKIE_SECRET
-if (!cookieSecret) {
-  throw new Error('COOKIE_SECRET environment variable is not set')
-}
-
-
-const ironOptions = {
-  cookieName: 'aces_session',
-  password: cookieSecret,
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
-    sameSite: 'strict'
-  },
-}
 
 type SessionData = {
   user: User | null
@@ -26,10 +11,23 @@ type SessionData = {
 }
 
 async function getSession(): Promise<IronSession<SessionData>> {
+  console.log('COOKIE_SECRET available:', !!process.env.COOKIE_SECRET)
+  const cookieSecret = process.env.COOKIE_SECRET
+  if (!cookieSecret) {
+    throw new Error('COOKIE_SECRET environment variable is not set')
+  }
+  const ironOptions = {
+    cookieName: 'aces_session',
+    password: cookieSecret,
+    cookieOptions: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      sameSite: 'strict'
+    },
+  }
   const cookieStore = cookies()
   const session = await getIronSession<SessionData>(cookieStore, ironOptions)
   return session
 }
 
 export default getSession
-export type { SessionData }
