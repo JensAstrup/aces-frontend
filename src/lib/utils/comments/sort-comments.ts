d2cc6f79dbb8474aeaf25bf8686e02cd68f2309c
Jens Astrup <jensaiden@gmail.com>
2024-09-25 22:58:28 -0400

    Improve comment for sorting

--- src/lib/utils/comments/sort-comments.ts
+++ src/lib/utils/comments/sort-comments.ts
@@ -4,7 +4,7 @@ import { Comment } from '@aces/interfaces/comment'
 
 
 function sortComments(comments: Comment[]): Comment[] {
-  // Sort comments by createdAt, descending
+  // Sort comments by createdAt, descending (newest at the top)
   return comments.sort((comment1, comment2) => {
     return dayjs(comment1.createdAt).unix() - dayjs(comment2.createdAt).unix()
   })
